e07b160983323d95c83d12c05e61da12922680fd
Menna Elbadry <mennatallah701@gmail.com>
2026-01-01 20:12:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+//task1: create variable type number 
+// let studentNumber = 210;
+// console.log(studentNumber);
+
+//keyboard printing 3
+// let keyboardPrice = 10;
+// let mousePrice = 5;
+
+// let keyboardSold = 7;
+// let mouseSold = 7;
+
+
+// let moneyKeyboard = (10 * 7);
+// console.log(10 * 7);
+// let totalMoney = (10*7);
+
+// naming your variables
+let y = true; 
+let fatherName = 'tariq';
+let fn = 'tariq';
+
+let fathernameforstudent;
+
+let _keyboards;
+let keyboard;
+let $keyboard;
+
+let ali;
+
+let student = 'Menna';
+
+// const PI = 3.14;
+// const birthYear = 2001;
+// birthYear = 2010;
+// const birthMonth = 4;
+// birthMonth = 7;
+
+let age = 24;
+age = 25;
+console.log(age);
+
+let radius = 10;
+radius = 7;
+radius = 10;
+
+console.log(radius)
+
+let birthDay = 10;
+birthDay = 20;
+console.log(birthDay);
+
+// ARTIMITIC OPERATORS (Math operators);
+
+
+let numbers = 10 - 5;
+let powerResult = 10**5;
+powerResult = 2**10;
+powerResult = 2*10
+powerResult = 2**3
+console.log(powerResult);
+
+let reminder = 23%2
+console.log(reminder)
+
+let increment = 1;
+increment++  //increment = increment +1
+increment++
+increment++
+increment++
+console.log(increment)
+
+let decrement = 5;
+decrement--  //decrement = decrement - 1
+decrement--
+decrement--
+decrement--
+decrement--
+console.log(decrement)
+
+
